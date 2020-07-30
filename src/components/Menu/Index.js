a3c0a7d4a4ac556767c37b31cabd21badfa46c04
Lucas Vinicius da Silva <lucasvns.silva@gmail.com>
2020-07-30 04:38:24 +0100

    Publicação Aula 2

--- src/components/Menu/Index.js
+++ src/components/Menu/Index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Logo from '../../assets/img/luvFlix.png'
 import './Menu.css'
+import {Link} from 'react-router-dom'
 //import ButtonLink from './components/Button/Index';
 import Button from '../Button'
 
 function Menu (){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className= "Logo" src={Logo} alt="AluraFlix lOGO"/>
-            </a>
+            </Link>
 
-            <Button as ="a" className="ButtonLink" href="/">
+            <Button as ={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video/index.js'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+ //Desafio Master Na descrição --- Criar um Componente da Página 404
+  const Pagina404 = () => (<div>Página 404</div>)
+  
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
