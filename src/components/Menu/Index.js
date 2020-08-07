ad6b7b14ea8129fc0342abda5b9233798a584e81
Lucas Vinicius da Silva <lucasvns.silva@gmail.com>
2020-08-07 02:52:41 +0100

    Commit Correção

--- src/components/Menu/Index.js
+++ src/components/Menu/Index.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import Logo from '../../assets/img/luvFlix.png'
-import './Menu.css'
-import {Link} from 'react-router-dom'
-//import ButtonLink from './components/Button/Index';
-import Button from '../Button'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu (){
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className= "Logo" src={Logo} alt="AluraFlix lOGO"/>
-            </Link>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-            <Button as ={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-
-        </nav>
-    )
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
