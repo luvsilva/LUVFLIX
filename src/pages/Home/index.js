1c4a31ee18f280a7d17868946736e63c62f0b101
Lucas Vinicius da Silva <lucasvns.silva@gmail.com>
2020-08-09 19:04:31 +0100

    Corrreção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 import styled from 'styled-components';
+import PageDefault from '../../components/PageDefault';
 
 const AppWrapper = styled.div`
   background: var(--grayDark);
@@ -12,38 +12,39 @@ const AppWrapper = styled.div`
 
 function Home() {
   return (
-    <AppWrapper>
-      <Menu></Menu>
-      <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-      url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"Teorias Filmes da Marvel - até que faz sentido !!!"}/>
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+    <PageDefault paddingAll={0}>
+      <AppWrapper>
+        <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Teorias Filmes da Marvel - até que faz sentido !!!"}/>
+        <Carousel
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[0]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+        <Carousel
+          category={dadosIniciais.categorias[1]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[2]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[3]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[4]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[5]}
+        />      
 
-      <Footer />
-    </AppWrapper>
+        <Footer />
+      </AppWrapper>
+    </PageDefault>
   );
 }
 
