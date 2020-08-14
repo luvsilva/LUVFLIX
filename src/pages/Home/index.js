921a178fc2252c9a0345f14b5b9a9fbaf447df35
Lucas Vinicius da Silva <lucasvns.silva@gmail.com>
2020-08-14 06:14:55 +0100

    Commit Funcional Videos + Categoria cadastrando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,49 +1,68 @@
-import React from 'react';
-import dadosIniciais from '../../data/dados_iniciais.json';
+import React,{useEffect, useState} from 'react';
+//import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 import styled from 'styled-components';
 import PageDefault from '../../components/PageDefault';
+import categoriasRepository from '../../repositories/categorias';
 
 const AppWrapper = styled.div`
   background: var(--grayDark);
 `;
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+
+  useEffect(() => {
+    // http://localhost:8080/categorias?_embed=videos
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos[0].videos[0]);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+
   return (
     <PageDefault paddingAll={0}>
-      <AppWrapper>
-        <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Teorias Filmes da Marvel - até que faz sentido !!!"}/>
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
+      
 
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />      
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <AppWrapper>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+              </AppWrapper>
+            </div>
+          );
+        }
 
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />      
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+         
 
-        <Carousel
-          category={dadosIniciais.categorias[4]}
-        />      
-
-        <Carousel
-          category={dadosIniciais.categorias[5]}
-        />      
-
-        <Footer />
-      </AppWrapper>
+     
     </PageDefault>
   );
 }
